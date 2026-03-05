b04a6934d03ef937987a6602356b3e236338741e
SourovKarmokar <sourovkarmokar22@gmail.com>
2026-03-05 19:49:59 +0600

    Routing route BrowserRouter and Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import RegistrationPage from './pages/RegistrationPage'
+import NoFoundPage from './pages/NoFoundPage'
 
 function App() {
   
-
+  
   return (
     <>
-    Tailwind Css
+    <Routes>
+      <Route element={<HomePage/>} path='/' exact />
+      <Route element={<LoginPage/>} path='/login' />
+      <Route element={<ProfilePage/>} path='/me' />
+      <Route element={<RegistrationPage/>} path='/register' />
+
+
+      <Route element={<NoFoundPage/>} path='*' />
+
+
+    </Routes>
     </>
   )
 }
