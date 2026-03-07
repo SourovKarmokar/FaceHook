9f46174a2352701196e3b2707358febd454eae3b
SourovKarmokar <sourovkarmokar22@gmail.com>
2026-03-07 22:57:46 +0600

    login

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,8 +1,17 @@
+import Field from "../common/Field";
+import { useForm } from "react-hook-form"
+
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
-    <div>
-      Login Form
-    </div>
+    <form className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]">
+      
+    </form>
   )
 }
 
